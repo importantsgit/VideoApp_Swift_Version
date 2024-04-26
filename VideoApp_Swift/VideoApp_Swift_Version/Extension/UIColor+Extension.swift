4ed00617ffce234eac2bbc3b4df1d38a72c17790
importantsgit <dlwognsdk@tukorea.ac.kr>
2024-04-26 11:53:57 +0900

    [이재훈] 이미지 폴더 정리 / UIColor+Extension 추가

---
+++ VideoApp_Swift/VideoApp_Swift_Version/Extension/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  VideoApp_Swift_Version
+//
+//  Created by 이재훈 on 4/26/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary          = UIColor(named: "Primary")
+    static let playerBackgorund = UIColor(named: "PlayerBackground")
+    static let refresh          = UIColor(named: "Refresh")
+}
